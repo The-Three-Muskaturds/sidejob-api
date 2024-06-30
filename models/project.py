0d86b0ea86cce1840f9dbebc4c5d1e07280c1e5c
Leander van Aarde <103223863+LeandervanAarde26@users.noreply.github.com>
2024-06-30 17:16:11 +0200

    fix(Projects): Projects model updated

--- models/project.py
+++ models/project.py
@@ -1,6 +1,6 @@
 from .base import BaseModel
 from datetime import datetime
-from sqlalchemy import Integer, String, DateTime, func
+from sqlalchemy import Integer, String, DateTime, func, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from .user import User
 
@@ -10,13 +10,19 @@ class Project(BaseModel):
 
     project_name: Mapped[String] = mapped_column(String(32), unique=False, nullable=False)
 
-    starting_at: Mapped[datetime] = mapped_column(
+    start_date: Mapped[datetime] = mapped_column(
         DateTime(timezone = True), default = func.utc_timestamp()
     )
         
-    ending_at: Mapped[datetime] = mapped_column(
+    end_date: Mapped[datetime] = mapped_column(
         DateTime(timezone = True), nullable = False,  default = func.utc_timestamp()
     )
-
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
     user: Mapped["User"] = relationship(back_populates="projects")
+
+    def __init__ (self, project_name, start_date, end_date, user_id):
+       self.project_name = project_name,
+       self.start_date = start_date
+       self.end_date = end_date
+       self.user_id = user_id
     
